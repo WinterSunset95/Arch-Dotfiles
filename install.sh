9a1fff240fe1115c2055c7bdcf20a8cc416c6190
WinterSunset95 <shirookamicod@gmail.com>
2022-03-18 00:58:37 +0530

    jksdfhlas

--- install.sh
+++ install.sh
@@ -4,30 +4,30 @@
 sudo pacman -S - < packages.txt
 
 #setting up the config
-mkdir ~/.config
-cd ~
-ln -s ~/dotfs/.xinitrc
-ln -s ~/dotfs/.fehbg
-cd ~/.config
-ln -s ~/dotfs/coc/
-ln -s ~/dotfs/nvim/
-ln -s ~/dotfs/guake/
-ln -s ~/dotfs/i3/
+#mkdir ~/.config
+#cd ~
+#ln -s ~/dotfs/.xinitrc
+#ln -s ~/dotfs/.fehbg
+#cd ~/.config
+#ln -s ~/dotfs/coc/
+#ln -s ~/dotfs/nvim/
+#ln -s ~/dotfs/guake/
+#ln -s ~/dotfs/i3/
 
 
 #setting up neovim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-sudo pacman -Syu
-nvim -c 'PackerInstall'
-cd ~/.config/coc/extensions
-npm install --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
-sudo npm install -g neovim
-pip install neovim
-nvim -c 'CocUpdate'
-nvim -c 'CocInstall coc-snippets coc-css coc-html coc-tsserver'
+#git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+#sudo pacman -Syu
+#nvim -c 'PackerInstall'
+#cd ~/.config/coc/extensions
+#npm install --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+#sudo npm install -g neovim
+#pip install neovim
+#nvim -c 'CocUpdate'
+#nvim -c 'CocInstall coc-snippets coc-css coc-html coc-tsserver'
 
 #setting up yay
-cd
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+#cd
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
