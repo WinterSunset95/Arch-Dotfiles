0948a026715ac6af6decc441c1c88e4f4b15c7ed
WinterSunset95 <wintersunset95@gmail.com>
2022-04-23 14:36:13 +0530

    latest update

--- install.sh
+++ install.sh
@@ -1,30 +1,68 @@
 #!/bin/bash
 
 #installing the required packages
-sudo pacman -S - < packages.txt
+#sudo pacman -S - < packages.txt
+dir=$(pwd) #get current working directory
 
 #setting up the config
-#mkdir ~/.config
-#cd ~
-#ln -s ~/dotfs/.xinitrc
-#ln -s ~/dotfs/.fehbg
-#cd ~/.config
-#ln -s ~/dotfs/coc/
-#ln -s ~/dotfs/nvim/
-#ln -s ~/dotfs/guake/
-#ln -s ~/dotfs/i3/
+# Startx
+cd /etc/X11/xinit
+printf "\nSetting up xinit"
+{
+	rm -rf xinitrc
+	printf "\nRemoved original xinitrc"
+} || {
+	printf "\nxinitrc not found\nLinking..."
+}
+{
+	ln -s $dir/xinitrc
+	printf "\nLinking successful"
+} || {
+	printf "\nLinking failed"
+} 
 
-
+# OpenBox
+printf "\nSetting up OpenBox"
+mmaker -vf OpenBox3
+cd /etc/xdg/
+{
+	rm -rf /etc/xdg/openbox/autostart
+	printf "\nRemoved original autostart file"
+} || {
+	printf "\n\n /etc/xdg/openbox/autostart not found!! \nLinking...\n "
+}
+{
+	ln -s $dir/autostart
+	printf "\nLinking successful"
+} || {
+	printf "\nLinking failed"
+}
 #setting up neovim
-#git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-#sudo pacman -Syu
-#nvim -c 'PackerInstall'
-#cd ~/.config/coc/extensions
-#npm install --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
-#sudo npm install -g neovim
-#pip install neovim
-#nvim -c 'CocUpdate'
-#nvim -c 'CocInstall coc-snippets coc-css coc-html coc-tsserver'
+# Bash version of try/catch
+printf "\Setting up neovim"
+cd /etc/xdg/
+{
+    rm -rf nvim
+    printf "\nRemoved nvim\nLinking..."
+} || {
+    printf "\nnvim not found\nLinking..."
+}
+{
+	ln -s $dir/nvim
+	printf "\nLinked successfully"
+} || {
+	printf "\nLinking failed"
+}
+
+{
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	printf "\nInstalled vim-plug"
+    printf "\nInstalling plugins"
+    nvim -c "PlugInstall"
+} || {
+	printf "\nvim-plug installation failed"
+}
 
 #setting up yay
 #cd
@@ -33,4 +71,4 @@ sudo pacman -S - < packages.txt
 #makepkg -si
 
 #backgrounds
-feh --bg-scale ~/dotfs/wallpapers/wp4504931-grunge-aesthetic-wallpapers.png
+#feh --bg-scale ~/dotfs/wallpapers/wp4504931-grunge-aesthetic-wallpapers.png
---
+++ nvim/init.vim
@@ -0,0 +1,41 @@
+set number
+set nowrap
+set cursorcolumn
+set cursorline
+set mouse=a
+set autoindent
+set laststatus=2
+set scrolloff=5
+set sidescrolloff=10
+set shiftround
+set shiftwidth=4
+set tabstop=4
+set softtabstop=4
+set wildmenu
+set wildmode=list:longest
+set clipboard=unnamedplus
+set splitbelow
+filetype plugin on
+set termguicolors
+
+call plug#begin()
+Plug 'dracula/vim'
+Plug 'mattn/emmet-vim'
+call plug#end()
+Plug 'dracula/vim', { 'as': 'dracula' }
+colorscheme dracula
+
+let g:mapleader = ','
+let g:user_emmet_leader_key = ','
+let g:netrw_banner = 0
+let g:netrw_liststyle = 3
+let g:netrw_browse_split = 4
+let g:netrw_altv = 1
+
+tnoremap <leader>t <c-\><c-n>
+
+au VimEnter * :Vexplore
+au VimEnter * :vertical resize 15
+au VimEnter * :set winfixwidth
+au VimEnter * :tabnew +term
+au VimEnter * :tabn
