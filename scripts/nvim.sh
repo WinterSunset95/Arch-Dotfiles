a4f98a3d58024af2ef4606b76db1dfa3af0adf7c
WinterSunset95 <wintersunset95@gmail.com>
2022-05-02 08:50:50 +0530

    Put my neovim configs in a seperate repository

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -5,33 +5,14 @@ boldred="\e[1;${red}m"
 boldcyan="\e[1;${cyan}m"
 end="\e[0m"
 
-dir=$(pwd)
 uid=$(id -u)
 user=$(whoami)
 
 printf "${boldred}\n\nSetting up neovim for $user ${end}\n"
 
-if [ uid == 0 ]
-then
-	cd /etc/xdg/
-else
-	cd ~/.config/
-fi
-{
-    rm -rf nvim
-	ln -s $dir/nvim
-} || {
-	ln -s $dir/nvim
-}
-printf "${boldcyan}\nDone!${end}"
-cd $dir
-printf "${boldred}\n\nInstall vim-plug? ${boldcyan}(Y/n)${end}"
-read opt
-
-if [ $opt == "n" ]
-then
-	printf " "
-else
-	bash ./nvim-plugins.sh
-fi
-
+cd ~
+rm -rf winter-nvim || printf "\n${boldred}winter-nvim doesn't exist... cloning${end}"
+git clone https://github.com/WinterSunset95/winter-nvim
+cd winter-nvim
+winter_dir=$(pwd)
+bash nvim.sh
